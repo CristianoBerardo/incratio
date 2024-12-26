3ac6b7d5e0782730675275d509066005769e5b5b
CristianoBerardo <cristianoberardo22@gmail.com>
2024-12-26 15:11:45 +0100

    start testing API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { ConvertRawData } from './tools/ConvertRawData';
+import yahooFinance from 'yahoo-finance2';
+
+const query = 'VWCE.MI';
+const queryOptions = { period1: '2021-02-01' };
+// const result = await yahooFinance.historical(query, queryOptions);
+
+// console.log(result);
 
 console.log('Hello, world!');
 console.log("I'm using nodemon and typescript");
 
-new ConvertRawData().processData();
+// new ConvertRawData().processData();
