3742cecf674b7ea4ad7861385d5bff4e9940c2e7
CristianoBerardo <cristianoberardo22@gmail.com>
2024-12-27 17:56:05 +0100

    Add express API to retrieve VWCE

--- apps/frontend/src/app/Routers/get.mjs
+++ apps/frontend/src/app/Routers/get.mjs
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import {
   array_analysis,
   histogram,
@@ -15,6 +18,21 @@ router.use(middleWare);
 
 router.get('/api/getsp500', (req, res) => {
   return res.status(200).send({ array_analysis, histogram });
+});
+
+router.get('/api/getVwce', async (req, res) => {
+  const __filename = fileURLToPath(import.meta.url);
+
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.resolve(
+    __dirname,
+    '../../../../../src/data/VWCE-MI.json'
+  );
+
+  const data = await fs.readFile(filePath, 'utf-8');
+
+  return res.status(200).send(JSON.parse(data));
 });
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import { writeFileSync } from 'fs';
 import yahooFinance from 'yahoo-finance2';
 
-const fun = async (): Promise<string>  => {
+const fun = async (): Promise<string> => {
   const query = 'VWCE.MI';
   const queryOptions = { period1: '2021-02-01' };
   const result = await yahooFinance.chart(query, queryOptions);
   console.log('\nDownloaded all data...\n');
-  console.log(result);
+  //console.log(result);
   writeFileSync('./src/data/VWCE-MI.json', JSON.stringify(result));
   return 'Write data on file';
 };
 
 // console.log(result);
 
-console.log('Hello, world!');
-console.log("I'm using nodemon and typescript");
 fun().then((res) => console.log(res));
 
 // new ConvertRawData().processData();
